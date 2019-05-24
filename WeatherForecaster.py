8563bdad7693534c597d522d392980bca101f65e
古川　拓実 <black3moon88@gmail.com>
2019-05-24 12:28:35 +0900

    AITの緯度経度から情報を取得するように変更

--- WeatherForecaster.py
+++ WeatherForecaster.py
@@ -10,18 +10,26 @@ from pytz import timezone
 
 # 天気を予想するクラス
 class WeatherForecaster:
-    def __init__(self, locate='470-0356,JP'):
+    def __init__(self, lat='35',lon='137'):
         self.api_key = 'b78fc959028d96ca6dffa19705903ee6'
-        self.api_page = 'http://api.openweathermap.org/data/2.5/forecast?zip={0}&units=metric&lang=ja&APPID={1}'
-        self.locate = locate
+        self.api_page = 'http://api.openweathermap.org/data/2.5/forecast?lat={0}&lon={1}&APPID={2}'
+        self.lat = lat
+        self.lon = lon
 
     # 三時間毎の天気を取得する
-    def getWeatherForecast(self):
+    def getJsonData(self):
         # {0},{1}の部分にそれぞれ場所とAPIキーを設定する
-        url = self.api_page.format(self.locate, self.api_key)
+        url = self.api_page.format(self.lat, self.lon, self.api_key)
+        print(url)
         req_response = requests.get(url)
         return json.loads(req_response.text)
 
+    def getCityData(self):
+        return self.getJsonData()["city"]
+
+    def getDataList(self):
+        return self.getJsonData()["list"]
+
 
 forecaster = WeatherForecaster()
-print(str( forecaster.getWeatherForecast()) )
+print(str( forecaster.getJsonData()) )
