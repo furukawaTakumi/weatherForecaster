0eb4581da93cbb902dbcf0afe0a88491079f2aa1
furukawaTakumi <baieruozisann@yahoo.co.jp>
2019-05-26 18:31:57 +0900

    cron-commandを利用して実行する形式に変更した

--- Main.py
+++ Main.py
@@ -12,22 +12,13 @@ twitter = Twitter.Twitter()
 
 def routine_work():
     twidata = forecaster.getTwitteData()
-    #twitter.twitte(contents.format(twidata.date, twidata.time, twidata.weather, twidata.temp, twidata.cloud_val, twidata.rain_val, twidata.wind_val))
+    twitter.twitte(contents.format(twidata.date, twidata.time, twidata.weather, twidata.temp, twidata.cloud_val, twidata.rain_val, twidata.wind_val))
 
-get_start = False
 while(True):
-    time_temp = dt.datetime.now()
-    print(time_temp.strftime('%H'))
-    if int(time_temp.strftime('%H')) % 3 == 0 and int(time_temp.strftime('%M')) == 0:
-        get_start = True
-        
-    if get_start:
         try:
             routine_work()
-            get_start = False
-            sleep(3600*2)
         except APIgetDataException(e):
             sleep(60)
             continue
-    else:
-        sleep(240)
+        
+    break
