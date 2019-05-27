b7151b8150cac14d37b641719de40fa108592d37
furukawaTakumi <baieruozisann@yahoo.co.jp>
2019-05-28 04:49:07 +0900

    時刻に対応させて、雲量表示の切り替えも出きるようにした

--- Main.py
+++ Main.py
@@ -5,18 +5,40 @@ import Twitter
 import APIgetDataException as apie
 from time import sleep
 
-contents = 'ラズパイ天気予報\n{0}\n{1}頃のAITは{2}になるでしょう！\n\n詳細\n 気温:{3}\n 雲量:{4}\n 降水量:{5}\n 風:{6}'
+def createContent(twidata):
+    contentLine1 = 'ラズパイ天気予報 ~{0}~\n'
+    contentLine2 = '{1}頃のAITは{2}になるでしょう！\n\n'
+
+    contentLine3 = '詳細\n'
+    contentLine4 = '　気温:{3}\n'
+    cnt = 4
+    contentLine5 = ""
+    if twidata.weather == "曇り":
+        contentLine5 = '　雲量:{4}\n'
+        cnt = cnt + 1
+    
+    contentLine6 = '　降水量:{' + str(cnt) + '}\n'
+    cnt = cnt + 1
+    contentLine7 = '　風:{' + str(cnt) + '}'
+    if cnt == 6:
+        return ( contentLine1 + contentLine2 + contentLine3 + contentLine4 + contentLine5 + contentLine6 + contentLine7).format(
+            twidata.date, twidata.time, twidata.weather, twidata.temp, twidata.cloud_val, twidata.rain_val, twidata.wind_val
+        )
+    else:
+        return ( contentLine1 + contentLine2 + contentLine3 + contentLine4 + contentLine6 + contentLine7).format(
+            twidata.date, twidata.time, twidata.weather, twidata.temp, twidata.rain_val, twidata.wind_val
+        )
+
 forecaster = wf.WeatherForecaster()
 twidata = td.Twidata()
 twitter = Twitter.Twitter()
 
 while(True):
-        try:
-            twidata = forecaster.getTwiiteData()
-            #twitter.twitte(contents.format(twidata.date, twidata.time, twidata.weather, twidata.temp, twidata.cloud_val, twidata.rain_val, twidata.wind_val))
-        except apie.APIgetDataException:
-            sleep(60)
-            continue
-        
-        print(contents.format(twidata.date, twidata.time, twidata.weather, twidata.temp, twidata.cloud_val, twidata.rain_val, twidata.wind_val))
-        break
+    try:
+        twidata = forecaster.getTwiiteData()
+        #twitter.twitte(createContent(twidata))
+    except apie.APIgetDataException:
+        sleep(60)
+        continue
+    print(createContent(twidata))
+    break
--- WeatherForecaster.py
+++ WeatherForecaster.py
@@ -35,9 +35,11 @@ class WeatherForecaster:
         date = data["dt_txt"]
         twidata.date = self.__getForecastDate()
         twidata.time = self.__getForecastTime()
-        twidata.weather = data["weather"][0]["description"]
+        twidata.weather = self.__weatherTranslator( data["weather"][0]["id"] )
         twidata.temp = str( "{:.2f}".format( float(data["main"]["temp"]) - 273.15) ) + "℃"
-        twidata.cloud_val = str( data["clouds"]["all"] ) + "％"
+        
+        if twidata.weather == "曇り":
+            twidata.cloud_val = self.__cloudsTranslator( data["weather"][0]["id"] )
 
         rain_val = 0
         if( "rain" in data and "3h" in data["rain"] ):
@@ -60,8 +62,31 @@ class WeatherForecaster:
             # utc時間に＋９することで日本時間になり、さらに三時間先の予報なので、12足している
             datetime.timezone(datetime.timedelta(hours=12))
         )
-        return jp_time.strftime("%H:00") 
+        return jp_time.strftime("%H:00")
+    
+    def __weatherTranslator(self, weatherID):
+        num = weatherID // 100
+        if num == 2:
+            return "雷雨"
+        if num == 3:
+            return "霧雨"
+        if num == 5:
+            return "雨"
+        if num == 6:
+            return "雪"
+        if num == 7:
+            return "特殊気候(霧など)"
+        if weatherID == 800:
+            return "晴天"
+        if num == 8:
+            return "曇り"
         
-
-wf = WeatherForecaster()
-wf.getTwiiteData()
+    def __cloudsTranslator(self, weatherID):
+        if weatherID == 801:
+            return "11-25%"
+        if weatherID == 802:
+            return "26-50%"
+        if weatherID == 803:
+            return "51-84%"
+        if weatherID == 804:
+            return "85-100%"
